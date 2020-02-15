6777eb9cd3a0a50aff4b048149fe1d0ee082ae3d
dannevernguyens <danguyenuw@gmail.com>
2020-02-15 14:44:19 -0800

    got task 1 and 2 mostly done so acping

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,17 @@
 'use strict';
 
-let likeButtons = document.getElementsByTagName('button');
+let likeButtons = $('button');
 
 for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener('click', likeMe);
+  // likeButtons[i].addEventListener('click', likeMe);
+  $(likeButtons[i]).on('click',likeMe);
 }
 
 function likeMe(e) {
   let character = e.target.parentNode;
   let counter = character.getElementsByTagName('span')[0];
+  // let counter = $('span:parent')[0];
+  // tried to garget with the above $, didnt work!
   let count = parseInt(counter.textContent);
   count++;
   counter.textContent = count;
